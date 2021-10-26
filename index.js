62098e13f0ec882d3a258a12a1e2780ceb4454cd
Fredrik Ridderfalk <ridderfalk@gmail.com>
2021-10-26 13:26:47 +0200

    grid created

--- index.js
+++ index.js
@@ -6,408 +6,74 @@ let moves = 0;
 let intervalTime = 100; //snake
 let speed = 0.8; //snake
 let timerId = 0; //snake
+let goalIndex = 0; //snake
+let direction = 1; //snake
 
 const layout = [
-  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-  1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0,
-  1, 1, 1, 1, 0, 1, 1, 3, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1,
-  1, 0, 1, 1, 1, 1, 3, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1,
-  1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1,
-  1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1,
-  1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1,
-  1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-  0, 1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-  1, 1, 0, 1, 1, 4, 1, 1, 1, 2, 2, 1, 1, 1, 4, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1,
-  1, 1, 1, 1, 0, 1, 1, 4, 1, 2, 2, 2, 2, 2, 2, 1, 4, 1, 1, 0, 1, 1, 1, 1, 1, 1,
-  4, 4, 4, 4, 4, 4, 0, 0, 0, 4, 1, 2, 2, 2, 2, 2, 2, 1, 4, 0, 0, 0, 4, 4, 4, 4,
-  4, 4, 1, 1, 1, 1, 1, 1, 0, 1, 1, 4, 1, 2, 2, 2, 2, 2, 2, 1, 4, 1, 1, 0, 1, 1,
-  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 4, 1, 1, 0,
-  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 4, 1,
-  1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-  4, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0,
-  1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1,
-  1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 3, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 3, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0,
-  1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1,
-  1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0,
-  0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1,
-  1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0,
-  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1,
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
   1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-  1, 1, 1, 1,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+  0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+  0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0,
 ];
 
-// Pacman game
-const width = 28;
-const grid = document.querySelector(".grid");
-const scoreDisplay = document.getElementById("score");
-let squares = [];
-let score = 0;
-
-// 0 - pacdots
-// 1 - wall
-// 2 - ghost lair
-// 3 - powerpellets
-// 4 - empty
-
-const layout = [
-  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-  1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0,
-  1, 1, 1, 1, 0, 1, 1, 3, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1,
-  1, 0, 1, 1, 1, 1, 3, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1,
-  1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1,
-  1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1,
-  1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1,
-  1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-  0, 1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-  1, 1, 0, 1, 1, 4, 1, 1, 1, 2, 2, 1, 1, 1, 4, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1,
-  1, 1, 1, 1, 0, 1, 1, 4, 1, 2, 2, 2, 2, 2, 2, 1, 4, 1, 1, 0, 1, 1, 1, 1, 1, 1,
-  4, 4, 4, 4, 4, 4, 0, 0, 0, 4, 1, 2, 2, 2, 2, 2, 2, 1, 4, 0, 0, 0, 4, 4, 4, 4,
-  4, 4, 1, 1, 1, 1, 1, 1, 0, 1, 1, 4, 1, 2, 2, 2, 2, 2, 2, 1, 4, 1, 1, 0, 1, 1,
-  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 4, 1, 1, 0,
-  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 4, 1,
-  1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-  4, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0,
-  1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1,
-  1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 3, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 3, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0,
-  1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1,
-  1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0,
-  0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1,
-  1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0,
-  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1,
-  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-  1, 1, 1, 1,
-];
-
-//create board
-function createBoard() {
+//create level
+function createLevel() {
   //for loop
   for (let i = 0; i < layout.length; i++) {
     //create a square
     const square = document.createElement("div");
     //put square in grid
-    grid.appendChild(square);
+    grid.append(square);
     //put square in squares array
     squares.push(square);
 
-    if (layout[i] === 0) {
-      squares[i].classList.add("pac-dot");
-    } else if (layout[i] === 1) {
+    if (layout[i] === 1) {
       squares[i].classList.add("wall");
     } else if (layout[i] === 2) {
-      squares[i].classList.add("ghost-lair");
-    } else if (layout[i] === 3) {
-      squares[i].classList.add("power-pellet");
+      squares[i].classList.add("goal");
     }
   }
 }
-createBoard();
+createLevel();
 
 // down - 40
 // up key - 38
 // left - 37
 // right - 39
 
-//starting position of pacman
-let pacmanCurrentIndex = 490;
-squares[pacmanCurrentIndex].classList.add("pacman");
+//starting position of player
+let playerCurrentIndex = 18;
+squares[playerCurrentIndex].classList.add("player");
 
-function control(e) {
-  squares[pacmanCurrentIndex].classList.remove("pacman");
-  switch (e.keyCode) {
-    case 40:
-      console.log("pressed down");
-      if (
-        !squares[pacmanCurrentIndex + width].classList.contains("ghost-lair") &&
-        !squares[pacmanCurrentIndex + width].classList.contains("wall") &&
-        pacmanCurrentIndex + width < width * width
-      )
-        pacmanCurrentIndex += width;
-      break;
-    case 38:
-      console.log("pressed up");
-      if (
-        !squares[pacmanCurrentIndex - width].classList.contains("ghost-lair") &&
-        !squares[pacmanCurrentIndex - width].classList.contains("wall") &&
-        pacmanCurrentIndex - width >= 0
-      )
-        pacmanCurrentIndex -= width;
-      break;
-    case 37:
-      console.log("pressed left");
-      if (
-        !squares[pacmanCurrentIndex - 1].classList.contains("ghost-lair") &&
-        !squares[pacmanCurrentIndex - 1].classList.contains("wall") &&
-        pacmanCurrentIndex % width !== 0
-      )
-        pacmanCurrentIndex -= 1;
-      if (pacmanCurrentIndex === 364) {
-        pacmanCurrentIndex = 391;
-      }
-      break;
-    case 39:
-      console.log("pressed right");
-      if (
-        !squares[pacmanCurrentIndex + 1].classList.contains("ghost-lair") &&
-        !squares[pacmanCurrentIndex + 1].classList.contains("wall") &&
-        pacmanCurrentIndex % width < width - 1
-      )
-        pacmanCurrentIndex += 1;
-      if (pacmanCurrentIndex === 391) {
-        pacmanCurrentIndex = 364;
-      }
-      break;
-  }
-  squares[pacmanCurrentIndex].classList.add("pacman");
-  pacDotEaten();
-  powerPelletEaten();
-  checkForWin();
-  checkForGameOver();
-}
-document.addEventListener("keyup", control);
-
-function pacDotEaten() {
-  if (squares[pacmanCurrentIndex].classList.contains("pac-dot")) {
-    squares[pacmanCurrentIndex].classList.remove("pac-dot");
-    score++;
-    scoreDisplay.innerHTML = score;
-  }
-}
-
-function powerPelletEaten() {
-  //if square pacman is in contains a power pellet
-  if (squares[pacmanCurrentIndex].classList.contains("power-pellet")) {
-    //remove power pellet class
-    squares[pacmanCurrentIndex].classList.remove("power-pellet");
-    //add a score of 10
-    score += 10;
-    //change each of the four ghosts to isScared
-    ghosts.forEach((ghost) => (ghost.isScared = true));
-    //use setTimeout to unscare ghosts after 10 seconds
-    setTimeout(unScareGhosts, 10000);
-  }
-}
-
-function unScareGhosts() {
-  ghosts.forEach((ghost) => (ghost.isScared = false));
-}
-
-class Ghost {
-  constructor(className, startIndex, speed) {
-    this.className = className;
-    this.startIndex = startIndex;
-    this.speed = speed;
-    this.currentIndex = startIndex;
-    this.isScared = false;
-    this.timerId = NaN;
-  }
-}
-
-const ghosts = [
-  new Ghost("blinky", 348, 250),
-  new Ghost("pinky", 376, 400),
-  new Ghost("inky", 351, 300),
-  new Ghost("clyde", 379, 500),
-];
-
-//draw my ghosts onto my grid
-ghosts.forEach((ghost) => {
-  squares[ghost.currentIndex].classList.add(ghost.className);
-  squares[ghost.currentIndex].classList.add("ghost");
-});
-
-//move the ghosts
-ghosts.forEach((ghost) => moveGhost(ghost));
-
-function moveGhost(ghost) {
-  console.log("moved ghost");
-  const directions = [-1, +1, -width, +width];
-  let direction = directions[Math.floor(Math.random() * directions.length)];
-  console.log(direction);
-
-  ghost.timerId = setInterval(function () {
-    //all our code
-    //if the next square does NOT contain a wall and does not contain a ghost
-    if (
-      !squares[ghost.currentIndex + direction].classList.contains("wall") &&
-      !squares[ghost.currentIndex + direction].classList.contains("ghost")
-    ) {
-      //remove any ghost
-      squares[ghost.currentIndex].classList.remove(ghost.className);
-      squares[ghost.currentIndex].classList.remove("ghost", "scared-ghost");
-      // //add direction to current Index
-      ghost.currentIndex += direction;
-      // //add ghost class
-      squares[ghost.currentIndex].classList.add(ghost.className);
-      squares[ghost.currentIndex].classList.add("ghost");
-    } else direction = directions[Math.floor(Math.random() * directions.length)];
-
-    //if the ghost is currently scared
-    if (ghost.isScared) {
-      squares[ghost.currentIndex].classList.add("scared-ghost");
-    }
-
-    //if the ghost is current scared AND pacman is on it
-    if (
-      ghost.isScared &&
-      squares[ghost.currentIndex].classList.contains("pacman")
-    ) {
-      //remove classnames - ghost.className, 'ghost', 'scared-ghost'
-      squares[ghost.currentIndex].classList.remove(
-        ghost.className,
-        "ghost",
-        "scared-ghost"
-      );
-      // change ghosts currentIndex back to its startIndex
-      ghost.currentIndex = ghost.startIndex;
-      //add a score of 100
-      score += 100;
-      //re-add classnames of ghost.className and 'ghost' to the ghosts new postion
-      squares[ghost.currentIndex].classList.add(ghost.className, "ghost");
-    }
-    checkForGameOver();
-  }, ghost.speed);
-}
-
-//check for game over
-function checkForGameOver() {
-  //if the square pacman is in contains a ghost AND the square does NOT contain a scared ghost
-  if (
-    squares[pacmanCurrentIndex].classList.contains("ghost") &&
-    !squares[pacmanCurrentIndex].classList.contains("scared-ghost")
-  ) {
-    //for each ghost - we need to stop it moving
-    ghosts.forEach((ghost) => clearInterval(ghost.timerId));
-    //remove eventlistener from our control function
-    document.removeEventListener("keyup", control);
-    //tell user the game is over
-    scoreDisplay.innerHTML = "You LOSE";
-  }
-}
-
-//check for win
-function checkForWin() {
-  if (score === 274) {
-    //stop each ghost
-    ghosts.forEach((ghost) => clearInterval(ghost.timerId));
+// If player has reached the goal, vortex away the player
+function goalReached() {
+  if (squares[playerCurrentIndex].classList.contains("player")) {
+    squares[playerCurrentIndex].classList.remove("player");
     //remove the eventListener for the control function
     document.removeEventListener("keyup", control);
-    //tell our user we have won
-    scoreDisplay.innerHTML = "You WON!";
+    //tell our player they beat the level
+    alert("Level cleared!");
   }
-}
-
-// Snake game
-const grid = document.querySelector(".grid");
-const startButton = document.getElementById("start");
-const scoreDisplay = document.getElementById("score");
-let squares = [];
-let currentSnake = [2, 1, 0];
-const snakeHead = currentSnake[0];
-let direction = 1;
-const width = 10;
-let appleIndex = 0;
-let score = 0;
-let intervalTime = 1000;
-let speed = 0.8;
-let timerId = 0;
-
-function createGrid() {
-  //create 100 of these elements with a for loop
-  for (let i = 0; i < width * width; i++) {
-    //create element
-    const square = document.createElement("div");
-    //add styling to the element
-    square.classList.add("square");
-    //put the element into our grid
-    grid.append(square);
-    //push it into a new squares array
-    squares.push(square);
-  }
-}
-createGrid();
-
-currentSnake.forEach((index) => squares[index].classList.add("snake"));
-
-function startGame() {
-  //remove the snake
-  currentSnake.forEach((index) => squares[index].classList.remove("snake"));
-  //remove the apple
-  squares[appleIndex].classList.remove("apple");
-  clearInterval(timerId);
-  currentSnake = [2, 1, 0];
-  score = 0;
-  //re add new score to browser
-  scoreDisplay.textContent = score;
-  direction = 1;
-  intervalTime = 1000;
-  generateApple();
-  //readd the class of snake to our new currentSnake
-  currentSnake.forEach((index) => squares[index].classList.add("snake"));
-  timerId = setInterval(move, intervalTime);
 }
 
 function move() {
   if (
-    (currentSnake[0] + width >= width * width && direction === width) || //if snake has hit bottom
-    (currentSnake[0] % width === width - 1 && direction === 1) || //if snake has hit right wall
-    (currentSnake[0] % width === 0 && direction === -1) || //if snake has hit left wall
-    (currentSnake[0] - width < 0 && direction === -width) || //if snake has hit top
-    squares[currentSnake[0] + direction].classList.contains("snake")
+    (playerCurrentIndex + width >= width * width && direction === width) || //if player has gone out bottom
+    (playerCurrentIndex % width === width - 1 && direction === 1) || //if player has gone out right
+    (playerCurrentIndex % width === 0 && direction === -1) || //if player has gone out left
+    (playerCurrentIndex - width < 0 && direction === -width) || //if player has gone out top
+    squares[playerCurrentIndex + direction].classList.contains("player")
   )
     return clearInterval(timerId);
 
-  //remove last element from our currentSnake array
-  const tail = currentSnake.pop();
-  //remove styling from last element
-  squares[tail].classList.remove("snake");
-  //add square in direction we are heading
-  currentSnake.unshift(currentSnake[0] + direction);
-  //add styling so we can see it
-
-  //deal with snake head gets apple
-  if (squares[currentSnake[0]].classList.contains("apple")) {
-    //remove the class of apple
-    squares[currentSnake[0]].classList.remove("apple");
-    //grow our snake by adding class of snake to it
-    squares[tail].classList.add("snake");
-    console.log(tail);
-    //grow our snake array
-    currentSnake.push(tail);
-    console.log(currentSnake);
-    //generate new apple
-    generateApple();
-    //add one to the score
-    score++;
-    //display our score
-    scoreDisplay.textContent = score;
-    //speed up our snake
-    clearInterval(timerId);
-    console.log(intervalTime);
-    intervalTime = intervalTime * speed;
-    console.log(intervalTime);
-    timerId = setInterval(move, intervalTime);
-  }
-
-  squares[currentSnake[0]].classList.add("snake");
+  squares[playerCurrentIndex].classList.add("player");
 }
-
-function generateApple() {
-  do {
-    appleIndex = Math.floor(Math.random() * squares.length);
-  } while (squares[appleIndex].classList.contains("snake"));
-  squares[appleIndex].classList.add("apple");
-}
-generateApple();
 
 function control(e) {
   if (e.keyCode === 39) {
@@ -425,7 +91,6 @@ function control(e) {
   }
 }
 document.addEventListener("keyup", control);
-startButton.addEventListener("click", startGame);
 
 window.addEventListener(
   "keydown",
