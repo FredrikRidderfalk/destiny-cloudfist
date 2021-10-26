0e0da29dabbf7592d4595ee647470429ef8b9d69
Fredrik Ridderfalk <ridderfalk@gmail.com>
2021-10-26 17:15:32 +0200

    HUD now updates properly

--- index.js
+++ index.js
@@ -77,19 +77,30 @@ function movePlayer(e) {
     if (squares[playerCurrentIndex].classList.contains("goal")) {
       squares[playerCurrentIndex].classList.remove("player");
       playerCurrentIndex = playerStartIndex;
+      direction = 0;
       squares[playerCurrentIndex].classList.add("player");
 
       //give player a cheer
       setTimeout(function () {
-        alert(`Level cleared in ${moves} moves!🥳🥳🥳🥳🥳`);
+        alert(
+          `Level cleared in ${moves} moves!🥳🥳🥳🥳🥳 Wow habibi, you're so brilliant, my record was  ${
+            moves + 1
+          }!😘`
+        );
+        resetGame();
       }, 10);
-      return (moves = 0);
     }
+
     // checkForEnteredDeepSpace() here too;
   }, speed);
 }
 
 document.addEventListener("keyup", movePlayer);
+
+function resetGame() {
+  moves = "";
+  movesDisplay.innerHTML = moves;
+}
 
 // this code prevents the window from scrolling around when pressing buttons in the game
 window.addEventListener(
